f226b7959821779223cc39518dc89aa30aec4bc6
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2021-07-24 20:40:04 +0700

    Fix

--- app/Http/Controllers/API/AccountController.php
+++ app/Http/Controllers/API/AccountController.php
@@ -11,6 +11,7 @@ use App\Http\Resources\Collection;
 use Auth;
 use App\Account;
 use App\BusinessSession;
+use App\Business;
 use App\Companies;
 use App\Employee;
 
@@ -28,16 +29,22 @@ class AccountController extends Controller
     {
         $user = Auth::guard('api')->user();
         
-        $session = BusinessSession::where('id_user', $user->id)->with('business')->first();
-        if (!$session) {
-          $employee = Employee::where('id_user', $user->id)->first();
-          $company = Companies::where('id', $employee->id_company)->first();
-          $session = BusinessSession::where('id_user', $company->id_user)->with('business')->first();
+        $isCompany = $user->hasRole('company');
+        if($isCompany){
+            $session = session('business');
+            $company = Companies::where('id_user', $user->id)->first()->id;
+            $business = Business::where('id_company', $company)->get();
+            if($session == null){
+                $session = Business::where('id_company', $company)->first();
+            }
+            $getBusiness = Business::with('company')
+            ->where('id_company', $company)->where('id', $session)->first();
+        } else {
+            $session = Employee::with('business')->where('id_user', $user->id)->first();
         }
-        if(!$session->business){
-          return response()->json(['success'=>false,'error'=>'Sesi bisnis belum dipilih.'], 400);
+        if(!$session){
+          return response()->json(['success'=>false,'error'=>'Sesi bisnis belum dipilih.'.$session], 400);
         }
-        $session = $session->business;
 
         $account = Account::select('id', 'id_classification', 'account_code', 'account_name', 'position')
         ->whereHas('classification.parent', function ($query) use ($session) {
